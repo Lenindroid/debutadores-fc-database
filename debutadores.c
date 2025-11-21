70df0c3e15e17d3d61daf42f7f7edc6ddf578181
Lenin Rojas Sánchez <leninrojassanchez09@gmail.com>
2025-11-21 11:35:00 -0600

    Primera version struct de partidos

--- debutadores.c
+++ debutadores.c
@@ -1,10 +1,29 @@
 #include<stdio.h>
+typedef int bool;
+#define true 1
+#define false 0
+
+struct Partido {
+    int jornada;
+    bool locales;
+    int golesAnotados;
+    int golesConcedidos;
+    char fase[32];
+    bool jugado;
+};
 
 int main(){
-    printf("En este momento todos ustedes seran hackeados\n");
-    printf("Todos ustedes\n");
-    printf("Hijos de pta\n");
-    printf("Hijos de pta\n");
-    printf("Hijos de pta\n");
+    int numeroDePartidos = 7;
+    struct Partido partidos[numeroDePartidos];
+
+    //Eliminar basura 
+    for (int i = 0; i < numeroDePartidos; i++) {
+        partidos[i].jornada = i + 1;
+        partidos[i].locales = true;
+        partidos[i].golesAnotados = 0;
+        partidos[i].golesConcedidos = 0;
+        snprintf(partidos[i].fase, sizeof partidos[i].fase, "Fase regular");
+        partidos[i].jugado = false;
+    }
     return 0;
 }
