2f439f6cc89d05fbfdb38c9a06f39edc0630516b
GialTona <gialtona@gmail.com>
2025-11-21 00:50:13 -0600

    pitufo

---
+++ futBot.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    printf("En este momento todos ustedes seran hackeados\n");
+    printf("Todos ustedes\n");
+    printf("Hijos de pta\n");
+    printf("Hijos de pta\n");
+    printf("Hijos de pta\n");
+    return 0;
+}
