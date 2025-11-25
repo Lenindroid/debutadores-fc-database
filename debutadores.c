2332039682f122784e67c8888a3f7de1ee3a1624
GialTona <gialtona@gmail.com>
2025-11-25 04:01:11 -0600

    Listo lenin, mi parte, ya hice toda la base de datos de los jugadores, yyy la uni rusticamente a la de los partidos, pero ahi esta el menu que queiras muak, me voy a dormir

--- debutadores.c
+++ debutadores.c
@@ -11,6 +11,20 @@ struct Dato {
     int partidosJugados;
     char posiciones [20];
 };
+
+struct Partido {
+    int jornada;
+    bool locales;
+    char rival[32];
+    int golesAnotados;
+    int golesConcedidos;
+    char fase[32];
+    bool jugado;
+};
+
+int main_jugadores();
+int main_partidos();
+
 void mostrarDatos(struct Dato *datos, int n, int in);
 void cargarDatos(struct Dato *datos);
 void form(struct Dato *datos, int index);
@@ -20,7 +34,32 @@ void eliminarJugador(struct Dato *datos, int *index, int el);
 int consultarJugador(struct Dato *datos, int index);
 void ciclo(struct Dato *datos, int *index);
 
+void cargarPartidos(struct Partido *partidos, int n);
+void mostrarPartidos(struct Partido *partidos, int n);
+void agregarPartido(struct Partido *partidos, int index);
+void modificarPartido(struct Partido *partidos, int index);
+void eliminarPartido(struct Partido *partidos, int index);
+
 int main(){
+    int opcion=0;
+    while(opcion!=3){
+        printf("--Debutadores FC--\n");
+        printf("Que desea hacer?\n1. Gestionar jugadores\n2. Gestionar partidos\n3. Salir\n");
+        scanf("%d", &opcion);
+        if (opcion == 1) {
+            main_jugadores();
+        } else if (opcion == 2) {
+            main_partidos();
+        } else if(opcion == 3){
+            break;
+        } else {
+            printf("Opcion no valida.\n");
+        }
+    }   
+    return 0;
+}
+
+int main_jugadores() {
     int index = 2;
     struct Dato datos[100];
     printf("--Base de datos de jugadores de Debutadores FC--\n");
@@ -185,4 +224,160 @@ void cargarDatos(struct Dato *datos) {
     datos[2].asistencias = 6;
     datos[2].partidosJugados = 8;
     snprintf(datos[2].posiciones, sizeof datos[2].posiciones, "Lateral derecho");
+}
+
+int main_partidos(){
+    int numeroDePartidos = 7;
+    struct Partido partidos[numeroDePartidos];
+    cargarPartidos(partidos, numeroDePartidos);
+    modificarPartido(partidos, 0);
+    mostrarPartidos(partidos, numeroDePartidos);
+    
+    return 0;
+}
+
+void cargarPartidos(struct Partido *partidos, int n) {
+    for (int i = 0; i < n; i++) {
+        partidos[i].jornada = i + 1;
+        partidos[i].locales = true;
+        snprintf(partidos[i].rival, sizeof partidos[i].rival, "No definido");
+        partidos[i].golesAnotados = 0;
+        partidos[i].golesConcedidos = 0;
+        snprintf(partidos[i].fase, sizeof partidos[i].fase, "Fase regular");
+        partidos[i].jugado = false;
+    }
+    
+    snprintf(partidos[0].rival, sizeof partidos[0].rival, "THE WOLFS");
+    partidos[0].golesAnotados = 7;
+    partidos[0].golesConcedidos = 6;
+    partidos[0].jugado = true;
+    
+    snprintf(partidos[1].rival, sizeof partidos[1].rival, "PAPIS INGE");
+    partidos[1].locales = false;
+    partidos[1].golesAnotados = 7;
+    partidos[1].golesConcedidos = 11;
+    partidos[1].jugado = true;
+    
+    snprintf(partidos[2].rival, sizeof partidos[2].rival, "THE WOLFS");
+    partidos[2].golesAnotados = 7;
+    partidos[2].golesConcedidos = 6;
+    partidos[2].jugado = true;
+}
+
+void mostrarPartidos(struct Partido *partidos, int n) {
+    printf("%-8s | %-20s | %-6s | %-20s | %-6s | %-20s | %-6s |\n",
+           "Jornada", "Local", "Goles", "Visitante", "Goles", "Fase", "Jugado");
+    printf("----------------------------------------------------------------------------------------------------\n");
+    for (int i = 0; i < n; i++) {
+        const char *local_name;
+        const char *visitante_name;
+        int goles_local;
+        int goles_visitante;
+
+        if (partidos[i].locales) {
+            local_name = "Debutadores FC";
+            visitante_name = partidos[i].rival;
+            goles_local = partidos[i].golesAnotados;
+            goles_visitante = partidos[i].golesConcedidos;
+        } else {
+            local_name = partidos[i].rival;
+            visitante_name = "Debutadores FC";
+            goles_local = partidos[i].golesConcedidos;
+            goles_visitante = partidos[i].golesAnotados;
+        }
+
+        printf("%-8d | %-20.20s | %-6d | %-20.20s | %-6d | %-20.20s | %-6s |\n",
+               partidos[i].jornada,
+               local_name,
+               goles_local,
+               visitante_name,
+               goles_visitante,
+               partidos[i].fase,
+               partidos[i].jugado ? "Si" : "No");
+    }
+}
+
+void modificarPartido(struct Partido *partidos, int index) {
+    printf("Est%cs modificando el siguiente partido:\n", 160);
+    printf("%-8s | %-20s | %-6s | %-20s | %-6s | %-20s | %-6s |\n",
+           "Jornada", "Local", "Goles", "Visitante", "Goles", "Fase", "Jugado");
+    printf("----------------------------------------------------------------------------------------------------\n");
+    int jugado_flag;
+    printf("%cEl partido ya se jug%c? (1-S%c, 0-No): ", 168, 162, 161);
+    scanf("%d", &jugado_flag);
+    if (jugado_flag == 0) 
+        partidos[index].jugado = false;
+    else
+        partidos[index].jugado = true;
+
+    if (!partidos[index].jugado) {
+        partidos[index].jugado = false;
+        partidos[index].jornada = index + 1;
+        partidos[index].locales = true;
+        snprintf(partidos[index].rival, sizeof partidos[index].rival, "No definido");
+        partidos[index].golesAnotados = 0;
+        partidos[index].golesConcedidos = 0;
+        snprintf(partidos[index].fase, sizeof partidos[index].fase, "Fase regular");
+        printf("El partido se marc%c como no jugado y se inicializaron los campos por defecto.\n", 162);
+        return;
+    }
+
+    const char *local_name;
+    const char *visitante_name;
+    int goles_local;
+    int goles_visitante;
+
+    if (partidos[index].locales) {
+        local_name = "Debutadores FC";
+        visitante_name = partidos[index].rival;
+        goles_local = partidos[index].golesAnotados;
+        goles_visitante = partidos[index].golesConcedidos;
+    } else {
+        local_name = partidos[index].rival;
+        visitante_name = "Debutadores FC";
+        goles_local = partidos[index].golesConcedidos;
+        goles_visitante = partidos[index].golesAnotados;
+    }
+
+    printf("%-8d | %-20.20s | %-6d | %-20.20s | %-6d | %-20.20s | %-6s |\n",
+        partidos[index].jornada,
+        local_name,
+        goles_local,
+        visitante_name,
+        goles_visitante,
+        partidos[index].fase,
+        partidos[index].jugado ? "Si" : "No");
+
+    int locales_opcion;
+    printf("%cJugamos como local? (1-Si, 0-No): ", 168);
+    scanf("%d", &locales_opcion);
+    if (locales_opcion == 0) 
+        partidos[index].locales = false;
+    else
+        partidos[index].locales = true;
+
+
+    printf("\n%cContra qui%cn jug%c nuestro equipo?:\n", 168, 130, 160);
+    scanf(" %31[^\n]", partidos[index].rival);
+    printf("%cCu%cntos goles anot%c nuestro equipo?:", 168, 130, 162);
+    scanf("%d", &partidos[index].golesAnotados);
+    printf("%cCu%cntos goles concedimos?:", 168, 130);
+    scanf("%d", &partidos[index].golesConcedidos);
+    int fase_opcion;
+    printf("Seleccione la fase del partido:\n1. Fase regular\n2. Fase de grupos\n3. Eliminatorias\n");
+    scanf("%d", &fase_opcion);
+    switch (fase_opcion) {
+        case 1:
+            snprintf(partidos[index].fase, sizeof partidos[index].fase, "Fase regular");
+            break;
+        case 2:
+            snprintf(partidos[index].fase, sizeof partidos[index].fase, "Fase de grupos");
+            break;
+        case 3:
+            snprintf(partidos[index].fase, sizeof partidos[index].fase, "Eliminatorias");
+            break;
+        default:
+            printf("Opci%cn no valida. Se mantiene la fase actual.\n", 162);
+            break;
+    }
 }
